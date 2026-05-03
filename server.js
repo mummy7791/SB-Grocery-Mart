68031a67543d9b2f1f614b8391079c87261a8a36
mummy7791 <arriyotechnologies@Gmail.com>
2026-05-03 22:27:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,12 +19,28 @@ app.use(cors());
 app.use(express.json());
 
 // Static files
+// Static files
 app.use(express.static(__dirname));
 
-app.use("/customer-app", express.static(path.join(__dirname, "../customer-app")));
-app.use("/admin-panel", express.static(path.join(__dirname, "../admin-panel")));
-app.use("/shop-app", express.static(path.join(__dirname, "../shop-app")));
-app.use("/delivery-boy-app", express.static(path.join(__dirname, "../delivery-boy-app")));
+app.use(
+  "/customer-app",
+  express.static(path.join(__dirname, "..", "customer-app"))
+);
+
+app.use(
+  "/admin-panel",
+  express.static(path.join(__dirname, "..", "admin-panel"))
+);
+
+app.use(
+  "/shop-app",
+  express.static(path.join(__dirname, "..", "shop-app"))
+);
+
+app.use(
+  "/delivery-boy-app",
+  express.static(path.join(__dirname, "..", "delivery-boy-app"))
+);
 const MONGO_URL =
   process.env.MONGO_URI ||
   "mongodb+srv://quickbasket:Quick123@cluster0.d4uhmxk.mongodb.net/quickbasket";
